0ee30b616a97569a412a60e7576d41edde2ee73c
Kashis Jawed <55854074+Kashish410@users.noreply.github.com>
2019-10-13 00:21:12 +0530

    updated it to 4

--- Lists_in_python.py
+++ Lists_in_python.py
@@ -4,13 +4,15 @@
 x = int(input())
 y = int(input())
 z = int(input())
+w = int(input())
 n = int(input())
 l=[]
 for i in range (0,x+1):
     for j in range (0,y+1):
         for k in range (0,z+1):
-            if i + j + k != n:
-                l1=[i,j,k]
+            for m in range (0, w+1):
+            if i + j + k + m != n:
+                l1=[i,j,k,m]
                 l.append(l1)
 print(l)                
 
